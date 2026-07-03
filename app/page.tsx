3d7893b918f10ef3c56fd794be07444761107b15
umerfarooq5349 <mumerfarooq557@gmail.com>
2026-07-03 11:45:10 +0500

    feat: mount ParallaxToolsSection2 on index page to enable testing of draggable logo canvas

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { Hero } from "@/components/sections/hero";
 import { Marquee } from "@/components/sections/marquee";
 import { AboutStats } from "@/components/sections/about-stats";
 import { ParallaxToolsSection } from "@/components/sections/parallax-tools";
+import { ParallaxToolsSection2 } from "@/components/sections/parallax-tools-2";
 import { ServicesBento } from "@/components/sections/services-bento";
 import { WorkGallery } from "@/components/sections/work-gallery";
 import { ProcessSteps } from "@/components/sections/process-steps";
@@ -15,6 +16,7 @@ export default function Home() {
       <Marquee />
       <AboutStats />
       <ParallaxToolsSection />
+      <ParallaxToolsSection2 />
       <ServicesBento />
       <WorkGallery />
       <ProcessSteps />
